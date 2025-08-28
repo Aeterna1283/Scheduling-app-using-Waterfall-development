950266a87e96b3f2ee61ccc5a7b80ee5c230ce8a
Aeterna1283 <rb9362123@gmail.com>
2025-08-28 13:38:49 -0400

    Schedules file is done

--- Student.java
+++ Student.java
@@ -1,16 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student
 {
     private String id;
     private String name;
-    private String course;
-    private boolean availability;
-    private String sessions;
+    private List<String> courses;
+    private List<String> availability;
+    private List<String> sessions;
 
-    public Student(String id, String name,String course,  boolean availability, String sessions)
+    public Student(String a_id, String a_name)
     {
+        this.id = a_id;
+        this.name = a_name;
+        this.courses = new ArrayList<>();
+        this.availability = new ArrayList<>();
+        this.sessions = new ArrayList<>();
+    }
 
+    public String getId()
+        {
+            return id;
+
+        }
+
+    public String getName()
+    {
+        return name;
 
     }
 
+    public List<String> getCourse()
+    {
+        return courses;
+
+    }
+
+    public List<String> getAvail()
+    {
+        return availability;
+
+    }
+
+    public List<String>getSessions()
+    {
+        return sessions;
+
+    }
+
+    void addCourse(String course)
+    {
+       courses.add(course);
+    }
+
+    void addAvailability(String time_slots)
+    {
+        availability.add(time_slots);
+    }
+
+    void removeAvailability(String time_slots)
+    {
+        availability.remove(time_slots);
+    }
+
+    void addSession(String sess)
+    {
+        sessions.add(sess);
+    }
 
 }
