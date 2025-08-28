55df67120a1fd7e0e76c7a29cdd573f985e26c5f
Aeterna1283 <rb9362123@gmail.com>
2025-08-28 13:21:14 -0400

    Started on student file

--- Student.java
+++ Student.java
@@ -1,3 +1,16 @@
-public class Student {
-    
+public class Student
+{
+    private String id;
+    private String name;
+    private String course;
+    private boolean availability;
+    private String sessions;
+
+    public Student(String id, String name,String course,  boolean availability, String sessions)
+    {
+
+
+    }
+
+
 }
