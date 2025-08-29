b3b34eecde63592972e2ed3456a9447b66806428
Aeterna1283 <rb9362123@gmail.com>
2025-08-29 09:13:00 -0400

    Started work on course.java

--- Course.java
+++ Course.java
@@ -1,3 +1,32 @@
-public class Course {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course
+{
+    private String courseId;
+    private String courseName;
+    private List<Student> enrolledStudents;
+
+    public Course(String cID, String cname)
+    {
+        this.courseId = cID;
+        this.courseName = cname;
+        this.enrolledStudents = new ArrayList<>();
+    }
+
+    public String getCourseId()
+    {
+        return courseId;
+    }
+
+    public String getcourseName()
+    {
+        return courseName;
+    }
+
+    public List<Student> getEnrolledStudents()
+    {
+        return enrolledStudents;
+    }
+
 }
