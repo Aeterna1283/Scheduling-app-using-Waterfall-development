a0d55997aba3c8a24da01260d5cbf37a8c1a35b4
Villalobos765 <rcv051999@gmail.com>
2025-08-29 09:29:35 -0400

    Update Availability.java

--- Availability.java
+++ Availability.java
@@ -10,4 +10,28 @@ public class Availability
         this.startTime = aStartTime;
         this.endTime = aEndTime;
     }
+
+    public boolean avaTime(Availability matchDay, Availability matchTime)
+    {
+        if(matchDay.day == this.day && matchTime.startTime == this.startTime)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    
+
+    public String getDay()
+        {
+            return day;
+        }
+    public String getStartTime()
+    {
+        return startTime;
+    }
+    public String getEndTime()
+    {
+        return endTime;
+    }
 }
