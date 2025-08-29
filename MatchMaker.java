bbbb1744d7d3d7a3de517ebcf6e20f3d084a0a47
Aeterna1283 <rb9362123@gmail.com>
2025-08-29 09:45:45 -0400

    started work on matchmaker
    finished 1/2 of the functions needed

--- MatchMaker.java
+++ MatchMaker.java
@@ -1,3 +1,23 @@
-public class MatchMaker {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatchMaker
+{
+    public List<Student> searchFriends(Student current, Course course)
+    {
+        List<Student> matches = new ArrayList<>();
+
+        for(Student stud : course.getEnrolledStudents())
+        {
+            if (stud != current)
+            {
+                matches.add(stud);
+            }
+
+        }
+        return matches;
+
+
+
+    }
 }
