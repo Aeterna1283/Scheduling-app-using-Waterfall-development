2f7774c1ac6224b909cdd334970046d210631b94
Aeterna1283 <rb9362123@gmail.com>
2025-08-29 10:58:50 -0400

    half way done with database

--- Database.java
+++ Database.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.List;
+
 public class Database
 {
     private String filename;
@@ -7,6 +10,38 @@ public class Database
         this.filename = fn;
 
     }
+
+    void saveStudents(List<Student> students) throws IOException {
+        FileWriter writer = new FileWriter(this.filename);
+
+        for(Student stud : students)
+        {
+            writer.write(stud.getId());
+            writer.write(stud.getName());
+
+            for(String cor : stud.getCourse())
+            {
+                writer.write(cor);
+
+            }
+
+            for(String avail : stud.getAvail())
+            {
+                writer.write(avail);
+
+            }
+
+            for(String sess : stud.getSessions())
+            {
+                writer.write(sess);
+
+            }
+        }
+
+        writer.close();
+
+    }
+
 
 
     
