e39286910d2780b03ceee36e7814400c957077ee
dmisyuk <david101cccp@gmail.com>
2025-08-31 12:11:03 -0400

    changes

--- Course.java
+++ Course.java
@@ -1,42 +1,42 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Course
-{
-    private String courseId;
-    private String courseName;
-    private List<Student> enrolledStudents;
-
-    public Course(String cID, String cname)
-    {
-        this.courseId = cID;
-        this.courseName = cname;
-        this.enrolledStudents = new ArrayList<>();
-    }
-
-    public String getCourseId()
-    {
-        return courseId;
-    }
-
-    public String getcourseName()
-    {
-        return courseName;
-    }
-
-    public List<Student> getEnrolledStudents()
-    {
-        return enrolledStudents;
-    }
-
-    void addStudent(Student s)
-    {
-        enrolledStudents.add(s);
-    }
-
-    void removeStudent(Student s)
-    {
-        enrolledStudents.remove(s);
-    }
-
-}
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Course
+//{
+//    private String courseId;
+//    private String courseName;
+//    private List<Student> enrolledStudents;
+//
+//    public Course(String cID, String cname)
+//    {
+//        this.courseId = cID;
+//        this.courseName = cname;
+//        this.enrolledStudents = new ArrayList<>();
+//    }
+//
+//    public String getCourseId()
+//    {
+//        return courseId;
+//    }
+//
+//    public String getcourseName()
+//    {
+//        return courseName;
+//    }
+//
+//    public List<Student> getEnrolledStudents()
+//    {
+//        return enrolledStudents;
+//    }
+//
+//    void addStudent(Student s)
+//    {
+//        enrolledStudents.add(s);
+//    }
+//
+//    void removeStudent(Student s)
+//    {
+//        enrolledStudents.remove(s);
+//    }
+//
+//}
--- Session.java
+++ Session.java
@@ -1,54 +1,54 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Session
-{
-    private String sessionID;
-    private Availability timeSlot;
-    List<Student> participants;
-    Course course;
-
-    public Session(String sID, Availability ts, Course cs)
-    {
-        this.sessionID = sID;
-        this.timeSlot = ts;
-        this.course = cs;
-        this.participants = new ArrayList<>();
-
-    }
-
-    public String getSessionID()
-    {
-        return sessionID;
-    }
-
-    public Availability getTimeSlot()
-    {
-        return timeSlot;
-    }
-
-    public List<Student> getParticipants()
-    {
-        return participants;
-    }
-
-    public Course getcourse()
-    {
-        return course;
-    }
-
-    void addParticipants(Student s)
-    {
-        participants.add(s);
-    }
-
-    void removeParticipants(Student s)
-    {
-        participants.remove(s);
-    }
-
-
-
-}
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Session
+//{
+//    private String sessionID;
+//    private Availability timeSlot;
+//    List<Student> participants;
+//    Course course;
+//
+//    public Session(String sID, Availability ts, Course cs)
+//    {
+//        this.sessionID = sID;
+//        this.timeSlot = ts;
+//        this.course = cs;
+//        this.participants = new ArrayList<>();
+//
+//    }
+//
+//    public String getSessionID()
+//    {
+//        return sessionID;
+//    }
+//
+//    public Availability getTimeSlot()
+//    {
+//        return timeSlot;
+//    }
+//
+//    public List<Student> getParticipants()
+//    {
+//        return participants;
+//    }
+//
+//    public Course getcourse()
+//    {
+//        return course;
+//    }
+//
+//    void addParticipants(Student s)
+//    {
+//        participants.add(s);
+//    }
+//
+//    void removeParticipants(Student s)
+//    {
+//        participants.remove(s);
+//    }
+//
+//
+//
+//}
 
 
