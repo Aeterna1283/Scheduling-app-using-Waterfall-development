2507e7a34408a56fedd94da99203be18e1b534ca
dmisyuk <david101cccp@gmail.com>
2025-08-31 13:32:47 -0400

    updated main with a menu

--- Main.java
+++ Main.java
@@ -2,60 +2,154 @@ import java.util.*;
 
 
 class Main {
-    public static ArrayList<Student> students = new ArrayList<>();
+    private static ArrayList<Student> students = new ArrayList<>();
+
+
     public static void main(String[] args)
     {
+        Student profile = null;
         Scanner sc = new Scanner(System.in);
-        //Documentation will serve as our input handling, lets not wast time with input validation
-        // Collect Student info to send to student
-        System.out.println("Student Name: ");
-        String name = sc.nextLine();
-        String id = sc.nextLine();
-        System.out.println("Student ID: ");
-        List<String> courses = new ArrayList<>();
-        System.out.println("Enter the courses you want a study buddy for:Example CPSC 3720, CPSC 3220 ");
-        String courInput = sc.nextLine();
-        String courArray[] = courInput.split(",");
-        for(String course : courArray)
-        {
-            courses.add(course.trim());
+        Integer menu;
+        System.out.println("Welcome to Study Buddy");
+        System.out.println("If you want add a new profile please press 1");
+        System.out.println("If you want to access an existing prifile press 2");
+        System.out.println("If you want to exit press 3");
+        menu = sc.nextInt();
+
+        switch (menu) {
+            case 1:
+                //Documentation will serve as our input handling, lets not wast time with input validation
+                // Collect Student info to send to student
+                System.out.println("Student Name: ");
+                String name = sc.nextLine();
+                System.out.println("Student ID: ");
+                String id = sc.nextLine();
+                List<String> courses = new ArrayList<>();
+                System.out.println("Enter the courses you want a study buddy for:Example CPSC 3720, CPSC 3220 ");
+                String courInput = sc.nextLine();
+                String courArray[] = courInput.split(",");
+                for(String course : courArray)
+                {
+                    courses.add(course.trim());
+                }
+
+                profile = new Student(id,name,courses);
+
+//                boolean addTime = true;
+//                while(addTime) {
+//                    System.out.println("Enter a day you can meet with a study budy: Example Monday");
+//                    String day = sc.nextLine();
+//                    System.out.println("Enter a start and end time to meet with a study buddy by the hour 0 to 23: Example 13 15");
+//                    Integer startTime = sc.nextInt();
+//                    Integer endTime = sc.nextInt();
+//                    sc.nextLine();
+//                    profile.addAvailability(day, startTime, endTime);
+//                    System.out.println("Do you want to add another time: Yes or No");
+//                    String logic = sc.nextLine();
+//                    if(logic.equalsIgnoreCase("no"))
+//                    {
+//                        addTime = false;
+//                    }
+//                }
+                addingAvail(profile);
+                students.add(profile);
+                break;
+            case 2:
+                System.out.println("Please enter you student ID");
+                String eID = sc.nextLine();
+
+                for(int i = 0; i < students.size(); i++)
+                {
+                    if(students.get(i).getId().equals(eID)){
+                        profile = students.get(i);
+
+                    }
+                }
+
+                if (profile == null)
+                {
+                    System.out.println("There is no student with that ID.");
+                    main(args);
+                }
+
+                System.out.println(profile.getAvailability());
+
+                System.out.println("Do you want to add availability?");
+                String inAns = sc.nextLine();
+                if(inAns.equalsIgnoreCase("yes")){
+                    addingAvail(profile);
+                }
+
+                System.out.println("Do you want to remove availability?");
+                inAns = sc.nextLine();
+                if(inAns.equalsIgnoreCase("yes"))
+                {
+                    removeAvail(profile);
+                }
+                break;
+            case 3:
+                System.exit(0);
         }
 
-        Student profile = new Student(id,name,courses);
 
-        boolean addTime = true;
-        while(addTime) {
-            System.out.println("Enter a day you can meet with a study budy: Example Monday");
-            String day = sc.nextLine();
-            System.out.println("Enter a start and end time to meet with a study buddy by the hour 0 to 23: Example 13 15");
-            Integer startTime = sc.nextInt();
-            Integer endTime = sc.nextInt();
-            sc.nextLine();
-            profile.addAvailability(day, startTime, endTime);
-            System.out.println("Do you want to add another time: Yes or No");
-            String logic = sc.nextLine();
-            if(logic.equalsIgnoreCase("no"))
-            {
-                addTime = false;
-            }
-        }
-        students.add(profile);
-
-        System.out.println("Would any more students like to find a study buddy?: Yes or No");
+        System.out.println("Do you want to find a sessions?");
         String reRun = sc.nextLine();
-        if (reRun.equalsIgnoreCase("yes"));
+        if (reRun.equalsIgnoreCase("no"));
         {
             main(args);
         }
-        if(reRun.equalsIgnoreCase("no"))
+        if(reRun.equalsIgnoreCase("yes"))
         {
-            System.out.println("BYE!");
-            System.exit(0);
+            if(students.size() < 2)
+            {
+                System.out.println("Sorry, no students to match.");
+                main(args);
+            }
+            MatchMaker matches = new MatchMaker(students);
+
+
+            main(args);
         }
     }
+
+    private static void addingAvail(Student stud) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a day you can meet with a study budy: Example Monday");
+        String day = sc.nextLine();
+        System.out.println("Enter a start and end time to meet with a study buddy by the hour 0 to 23: Example 13 15");
+        Integer startTime = sc.nextInt();
+        Integer endTime = sc.nextInt();
+        sc.nextLine();
+        stud.addAvailability(day, startTime, endTime);
+        System.out.println("Do you want to add another time: Yes or No");
+        String logic = sc.nextLine();
+        if(logic.equalsIgnoreCase("no"))
+        {
+            return;
+        }
+        addingAvail(stud);
+
+    }
+
+    private static void removeAvail(Student stud){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a day you want to remove time availability from: Example Monday");
+        String day = sc.nextLine();
+        System.out.println("Enter a start and end time to remove 0 to 23: Example 13 15");
+        Integer startTime = sc.nextInt();
+        Integer endTime = sc.nextInt();
+        sc.nextLine();
+        stud.removeAvailability(day, startTime, endTime);
+        System.out.println("Do you want to remove another time: Yes or No");
+        String logic = sc.nextLine();
+        if(logic.equalsIgnoreCase("no"))
+        {
+            return;
+        }
+        removeAvail(stud);
+    }
+
 }
-
-
 
 //BEGIN MAIN
 //
